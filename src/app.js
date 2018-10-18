7c78be8c0d4969a0f302c3a1d2ef3f3e1f77b3b9
Dwight Padilla <dwight.padilla@jobandesther.com>
2018-10-18 10:58:12 +0300

    Added src directory

---
+++ src/app.js
@@ -0,0 +1,143 @@
+const DatabaseManager = require("./dbmanager")
+const db = new DatabaseManager("./transactionsdb.sqlite3", (err) => {
+	if(err) {
+		return
+	}
+	const express = require("express")
+	const bodyParser = require('body-parser')
+	const app = express()
+	const port = 8080
+	app.use(bodyParser.json())
+	app.get("/", function(req, res) {
+		db.getAll((err, rows) => {
+			if(err) {
+				res.json(err)
+				return
+			}
+			res.json({
+				status: "ok",
+				data: rows
+			})
+		})
+	})
+	app.put("/:transactionId", function(req, res) {
+		var v = JSON.parse(JSON.stringify(req.body))
+		var err = validateAndSanitizeData(v)
+		if(err) {
+			res.json(err)
+			return
+		}
+		db.update(req.params.transactionId, v.amount, v.type, v.description, (err2) => {
+			if(err2) {
+				resz.json(err2)
+				return
+			}
+			res.json({
+				status: "ok"
+			})
+		})
+	})
+	app.delete("/:transactionId", function(req, res) {
+		db.delete(req.params.transactionId, (err) => {
+			if(err) {
+				resz.json(err)
+				return
+			}
+			res.json({
+				status: "ok"
+			})
+		})
+	})
+	app.post("/", function(req, res) {
+		var v = JSON.parse(JSON.stringify(req.body))
+		var err = validateAndSanitizeData(v)
+		if(err) {
+			res.json(err)
+			return
+		}
+		db.add(v.amount, v.type, v.description, (err2) => {
+			if(err2) {
+				res.json(err2)
+				return
+			}
+			res.json({
+				status: "ok"
+			})
+		})
+	})
+	app.get("/:transactionId", function(req, res) {
+		db.get(req.params.transactionId, (err, row) => {
+			if(err) {
+				res.json(err)
+				return
+			}
+			res.json({
+				status: "ok",
+				data: row
+			})
+		})
+	})
+	app.listen(port)
+	console.log("RESTful Wallet API server started on port: " + port)
+})
+
+function validateAndSanitizeData(data) {
+	if(data) {
+		var v = {}
+		if(data.amount) {
+			var i
+			for(i = 0; i < data.amount.length; i++) {
+				var c = data.amount.charCodeAt(i)
+				if((c >= 48 && c <= 57) || (c == 46)) {
+				}
+				else {
+					return {
+						status: "error",
+						code: "invalidAmount"
+					}
+				}
+			}
+			v["amount"] = data.amount
+		}
+		else {
+			return {
+				status: "error",
+				code: "noAmount"
+			}
+		}
+		if(data.type) {
+			if(data.type === "deduction" || data.type === "addition") {
+				v["type"] = data.type
+			}
+			else {
+				return {
+					status: "error",
+					code: "invalidType"
+				}
+			}
+		}
+		else {
+			return {
+				status: "error",
+				code: "noType"
+			}
+		}
+		if(data.description) {
+			v["description"] = data.description
+		}
+		else {
+			return {
+				status: "error",
+				code: "noDescription"
+			}
+		}
+		data = v
+	}
+	else {
+		return {
+			status: "error",
+			code: "noData"
+		}
+	}
+	return null
+}
---
+++ src/dbmanager.js
@@ -0,0 +1,116 @@
+const sqlite3 = require("sqlite3")
+
+class DatabaseManager {
+	constructor(file, callback) {
+		this.db = new sqlite3.Database(file, (err) => {
+			if(err) {
+				console.log('Could not connect to SQLite database', err)
+			}
+			else {
+				console.log('Connected to SQLite database')
+				this.initialize((err2) => {
+					if(err2) {
+						console.log('Failed to create database table', err2)
+					}
+					callback(err2)
+				})
+				return
+			}
+			callback(err)
+		})
+	}
+
+	initialize(callback) {
+		this.db.run("CREATE TABLE IF NOT EXISTS transactions (transactionId TEXT PRIMARY KEY, amount BIGINT, type VARCHAR(255), description TEXT);", callback)
+	}
+
+	generateTransactionId() {
+		var id = ""
+		var length = 10
+		var i
+		for(i = 0; i < length; i++) {
+			id = id.concat((Math.floor(Math.random() * 10)).toString())
+		}
+		return id
+	}
+
+	add(amount, type, description, callback) {
+		this.db.run("INSERT INTO transactions (transactionId, amount, type, description) VALUES (?, ?, ?, ?);", [ this.generateTransactionId(), this.convertToLong(amount), type, description ], callback)
+	}
+
+	update(transactionId, amount, type, description, callback) {
+		this.db.run("UPDATE transactions SET amount = ?, type = ?, description = ? WHERE transactionId = ?;", [ this.convertToLong(amount), type, description, transactionId ], callback)
+	}
+
+	delete(transactionId, callback) {
+		this.db.run("DELETE FROM transactions WHERE transactionId = ?;", [ transactionId ], callback)
+	}
+
+	get(transactionId, callback) {
+		this.db.get("SELECT * FROM transactions WHERE transactionId = ?;", [ transactionId ], (err, row) => {
+			if(err) {
+				callback(err, null)
+				return
+			}
+			else if(row) {
+				row.amount = this.convertToString(row.amount)
+			}
+			callback(null, row)
+		})
+	}
+	
+	getAll(callback) {
+		this.db.all("SELECT * FROM transactions;", (err, rows) => {
+			if(err) {
+				callback(err, null)
+				return
+			}
+			else if(rows) {
+				var i
+				for(i = 0; i < rows.length; i++) {
+					rows[i].amount = this.convertToString(rows[i].amount)
+				}
+			}
+			callback(null, rows)
+		})
+	}
+
+	convertToString(amount) {
+		if(amount) {
+			return parseFloat(amount / 100).toFixed(2).toString()
+		}
+		return "0.00"
+	}
+
+	convertToLong(amount) {
+		if(amount && /\S/.test(amount)) {
+			var i = amount.indexOf(".")
+			if(i > -1) {
+				var diff = (amount.length - 1) - i
+				if(diff > 2) {
+					if(parseInt(amount.charAt(i + 3)) > 4) {
+						var d = parseFloat(amount) + 0.01
+						amount = d.toString()
+					}
+					amount = amount.substring(0, amount.indexOf(".") + 3)
+				}
+				else if (diff > 1) {
+				}
+				else if (diff > 0) {
+					amount = amount + "0"
+				}
+				else if (diff > -1) {
+					amount = amount + "00"
+				}
+				while(amount.indexOf(".") > -1) {
+					amount = amount.replace(".", "")
+				}
+				return parseInt(amount)
+			}
+			return parseInt(amount + "00")
+		}
+		return 0
+	}
+}
+
+module.exports = DatabaseManager
